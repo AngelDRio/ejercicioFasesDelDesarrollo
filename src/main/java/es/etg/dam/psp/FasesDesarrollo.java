c7530e67c5ecc66bdb39603c7280f29beafd8a2a
AngelDRio <angeldelrio1802@gmail.com>
2025-11-15 22:53:57 +0100

    programa con constantes

--- src/main/java/es/etg/dam/psp/FasesDesarrollo.java
+++ src/main/java/es/etg/dam/psp/FasesDesarrollo.java
@@ -1,36 +1,53 @@
 package es.etg.dam.psp;
 
 public class FasesDesarrollo {
+
+    private static final String TEXT_INICIANDO = "Inciando desarrollo de software";
+    private static final String TEXT_EJECUTANDO = "Ejecutando ";
+    private static final String TEXT_FINALIZADO = " finalizado";
+    private static final String TEXT_TODAS_FASES_FINALIZADAS = "Todas las fases del desarrollo han finalizado";
+
+    private static final String NOM_ANALISIS = "Análisis";
+    private static final String NOM_DISENO = "Diseño";
+    private static final String NOM_CODIFICACION = "Codificación";
+    private static final String NOM_PRUEBAS = "Pruebas";
+
+    private static final int TIEMPO_ESPERA_ANALISIS = 3000;
+    private static final int TIEMPO_ESPERA_DISENO = 2000;
+    private static final int TIEMPO_ESPERA_CODIFICACION = 1000;
+    private static final int TIEMPO_ESPERA_PRUEBAS = 2000;
     public static void main(String[] args) throws InterruptedException{
 
-        Tarea t1 = new Tarea("Análisis",3000);
-        Tarea t2 = new Tarea("Diseño",2000);
-        Tarea t3 = new Tarea("Codificación",1000);
-        Tarea t4 = new Tarea("Pruebas",2000);
+        Tarea t1 = new Tarea(NOM_ANALISIS, TIEMPO_ESPERA_ANALISIS);
+        Tarea t2 = new Tarea(NOM_DISENO, TIEMPO_ESPERA_DISENO);
+        Tarea t3 = new Tarea(NOM_CODIFICACION, TIEMPO_ESPERA_CODIFICACION);
+        Tarea t4 = new Tarea(NOM_PRUEBAS, TIEMPO_ESPERA_PRUEBAS);
         
-        System.out.println("Inciando desarrollo de software");
+        System.out.println(TEXT_INICIANDO);
         
-        System.out.println("Ejecutando análisis");
+        System.out.println(TEXT_EJECUTANDO + NOM_ANALISIS);
         t1.start();
 
         t1.join();
-        System.out.println("Análisis finalizado");
+        System.out.println(NOM_ANALISIS + TEXT_FINALIZADO);
 
-        System.out.println("Ejecutando diseño");
+        System.out.println(TEXT_EJECUTANDO + NOM_DISENO);
         t2.start();
 
         t2.join();
-        System.out.println("Diseño finalizado");
+        System.out.println(NOM_DISENO + TEXT_FINALIZADO);
 
-        System.out.println("Ejecutando codificación");
+        System.out.println(TEXT_EJECUTANDO + NOM_CODIFICACION);
         t3.start();
 
         t3.join();
-        System.out.println("Codificación finalizada");
+        System.out.println(NOM_CODIFICACION + TEXT_FINALIZADO);
         
-        System.out.println("Ejecutando pruebas");
+        System.out.println(TEXT_EJECUTANDO + NOM_PRUEBAS);
         t4.start();
 
-        System.out.println("Desarrollo finalizado");
+        System.out.println(NOM_PRUEBAS + TEXT_FINALIZADO);
+
+        System.out.println(TEXT_TODAS_FASES_FINALIZADAS);
     }
 }
